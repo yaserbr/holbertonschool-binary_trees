b0d62f8e73694c913c8ac9578df0227985500096
Hassan <7soon721@gmail.com>
2026-08-19 00:19:59 +0300

    Add task 10 file, update binary_trees.h file

--- binary_trees.h
+++ binary_trees.h
@@ -26,5 +26,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
