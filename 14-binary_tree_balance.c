7dfa82d656f300e787573f7070d43979ee8eee5f
Hassan <7soon721@gmail.com>
2026-08-19 14:12:38 +0300

    Fix: compile error on server side

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, or 0 if tree is NULL
+ */
+size_t tree_height_helper(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = tree_height_helper(tree->left) + 1;
+	if (tree->right)
+		right_height = tree_height_helper(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure the balance factor
  *
@@ -15,10 +37,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left_height = (int)binary_tree_height(tree->left) + 1;
+		left_height = (int)tree_height_helper(tree->left) + 1;
 
 	if (tree->right)
-		right_height = (int)binary_tree_height(tree->right) + 1;
+		right_height = (int)tree_height_helper(tree->right) + 1;
 
 	return (left_height - right_height);
 }
