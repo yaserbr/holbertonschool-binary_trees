e75706018b7d6e904cb2839e9c06aaba7d8b7682
Hassan <7soon721@gmail.com>
2026-08-19 14:14:45 +0300

    Fix: Betty style coding

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * tree_height_helper - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  *
  * Return: Height of the tree, or 0 if tree is NULL
