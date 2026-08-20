c38a1f93e3c29f47b5e8ba4d2023f3e0c7fb2fc6
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-20 17:00:33 +0300

    8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,12 +4,13 @@
  * @tree: Pointer to the root node of the tree
  * @func: Pointer to a function that prints a number
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, func);
 }
+    
